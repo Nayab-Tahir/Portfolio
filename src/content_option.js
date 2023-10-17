48e3822d4d4816f302b156a4e487b1f84ea8c256
Nayab-Tahir <nayabtahir321@gmail.com>
2023-10-18 01:56:26 +0500

    First Chnage

--- src/content_option.js
+++ src/content_option.js
@@ -1,11 +1,11 @@
-const logotext = "JOHN";
+const logotext = "Nayab";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Nayab Tahir",
+    description: "I’m Nayab Tahir data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Nayab Tahir",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
